7ac9d5f97b5fa47c8871fce8a6576a3b5e7074c9
daniil-b2be <dan.developer@born2be.media>
2025-02-25 10:14:23 +0200

    where to start section

---
+++ src/app/components/where-to-start/where-to-start.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+
+import { ArrowTopRight } from '@/shared/ui/icons';
+import { Button } from '@/shared/ui/kit/button';
+import { Text } from '@/shared/ui/kit/text';
+import { Title } from '@/shared/ui/kit/title';
+
+import st from './where-to-start.module.scss';
+
+export function WhereToStart() {
+  return (
+    <section className={st.layout}>
+      <section>
+        <Title level={4} color="darkBlue" weight={500}>
+          Not Sure Where to Start?
+        </Title>
+        <Title level={4} color="darkBlue" weight={500}>
+          Need a Complete Solution?
+        </Title>
+      </section>
+      <section>
+        <Image
+          className={st.imgBanner}
+          src="/where-to-start.svg"
+          alt="where-to-start-banner"
+          width={1200}
+          height={400}
+          layout="responsive"
+        />
+      </section>
+      <section className={st.footer}>
+        <Text color="lightBlue" className={st.textLayout}>
+          Find the right path for your business. Choose a solution that best
+          fits your idea and goals.
+        </Text>
+        <CheckSolutionBtn />
+      </section>
+    </section>
+  );
+}
+
+function CheckSolutionBtn() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <Button
+      className={st.checkBtn}
+      variant="black"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <span>Check Growth Solutions</span>
+      <ArrowTopRight color={isHovered ? 'black' : 'white'} />
+    </Button>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from './components/hero';
 import { HowHelps } from './components/how-helps';
+import { WhereToStart } from './components/where-to-start';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <HowHelps />
+      <WhereToStart />
     </main>
   );
 }
